f4c2331276621f94cbc2157a6009fa4fa6dde7ff
xmoe <10909416+xmoezzz@users.noreply.github.com>
2022-12-24 14:09:35 +0800

    add patch: empty x embryo

---
+++ Patch/EmptyXEmbryo_chs/EmptyXEmbryo_chs/IStreamExXP3.cpp
@@ -0,0 +1,346 @@
+#include "IStreamExXP3.h"
+
+IStreamAdapterXP3::IStreamAdapterXP3(StreamHolderXP3 *ref)
+{
+	Stream = ref;
+	RefCount = 1;
+}
+//---------------------------------------------------------------------------
+IStreamAdapterXP3::~IStreamAdapterXP3()
+{
+	delete Stream;
+}
+//---------------------------------------------------------------------------
+HRESULT STDMETHODCALLTYPE IStreamAdapterXP3::QueryInterface(REFIID riid,
+	void **ppvObject)
+{
+	if (!ppvObject) return E_INVALIDARG;
+
+	*ppvObject = NULL;
+	if (!memcmp(&riid, &IID_IUnknown, 16))
+		*ppvObject = (IUnknown*)this;
+	else if (!memcmp(&riid, &IID_ISequentialStream, 16))
+		*ppvObject = (ISequentialStream*)this;
+	else if (!memcmp(&riid, &IID_IStream, 16))
+		*ppvObject = (IStream*)this;
+
+	if (*ppvObject)
+	{
+		AddRef();
+		return S_OK;
+	}
+	return E_NOINTERFACE;
+}
+//---------------------------------------------------------------------------
+ULONG STDMETHODCALLTYPE IStreamAdapterXP3::AddRef(void)
+{
+	return ++RefCount;
+}
+//---------------------------------------------------------------------------
+ULONG STDMETHODCALLTYPE IStreamAdapterXP3::Release(void)
+{
+	if (RefCount == 1)
+	{
+		delete this;
+		return 0;
+	}
+	else
+	{
+		return --RefCount;
+	}
+}
+
+#include "Se.h"
+
+
+inline PLDR_MODULE GetKrMovieLdr()
+{
+	LDR_MODULE *Ldr, *FirstLdr;
+
+	Ldr = FIELD_BASE(Nt_CurrentPeb()->Ldr->InInitializationOrderModuleList.Flink, LDR_MODULE, InInitializationOrderLinks);
+	FirstLdr = Ldr;
+
+	do
+	{
+		Ldr = FIELD_BASE(Ldr->InInitializationOrderLinks.Flink, LDR_MODULE, InInitializationOrderLinks);
+		if (Ldr->BaseDllName.Buffer == NULL)
+			continue;
+
+		if (CHAR_UPPER4W(*(PULONG64)(Ldr->BaseDllName.Buffer + 0)) != TAG4W('KRMO') ||
+			CHAR_UPPER4W(*(PULONG64)(Ldr->BaseDllName.Buffer + 4)) != CHAR_UPPER4W(TAG4W('VIE.')))
+		{
+			continue;
+		}
+
+		return Ldr;
+
+	} while (FirstLdr != Ldr);
+
+	return NULL;
+}
+
+
+inline PLDR_MODULE GetModuleLdr(PVOID ModuleBase)
+{
+	LDR_MODULE *Ldr, *FirstLdr;
+
+	Ldr = FIELD_BASE(Nt_CurrentPeb()->Ldr->InInitializationOrderModuleList.Flink, LDR_MODULE, InInitializationOrderLinks);
+	FirstLdr = Ldr;
+
+	do
+	{
+		Ldr = FIELD_BASE(Ldr->InInitializationOrderLinks.Flink, LDR_MODULE, InInitializationOrderLinks);
+		if (Ldr->BaseDllName.Buffer == NULL)
+			continue;
+
+		if (Ldr->DllBase != ModuleBase)
+			continue;
+
+		return Ldr;
+
+	} while (FirstLdr != Ldr);
+
+	return NULL;
+}
+
+
+BOOL IsValidDllWorkerThread(PVOID ThreadStart)
+{
+	PVOID       LayerExMovieHandle;
+	PVOID       KrMovieHandle;
+	PLDR_MODULE LayerExMovieLdr, KrMovieLdr;
+	BOOL        Result;
+
+	LayerExMovieHandle = Nt_GetModuleHandle(L"layerExMovie.dll");
+	KrMovieHandle      = Nt_GetModuleHandle(L"krmovie.dll");
+
+	if (LayerExMovieHandle == NULL && KrMovieHandle == NULL)
+		return FALSE;
+
+	LOOP_ONCE
+	{
+		Result = FALSE;
+		if (LayerExMovieHandle)
+		{
+			LayerExMovieLdr = GetModuleLdr(LayerExMovieHandle);
+			if (!LayerExMovieLdr)
+				break;
+
+			if (ThreadStart < LayerExMovieLdr->DllBase || ThreadStart >((PBYTE)LayerExMovieLdr->DllBase + LayerExMovieLdr->SizeOfImage))
+				break;
+
+			Result = TRUE;
+		}
+	}
+
+	if (Result)
+		return Result;
+
+	LOOP_ONCE
+	{
+		if (KrMovieHandle)
+		{
+			KrMovieLdr = GetModuleLdr(KrMovieHandle);
+			if (!KrMovieLdr)
+				break;
+
+			if (ThreadStart >= KrMovieLdr->DllBase || ThreadStart <= ((PBYTE)LayerExMovieLdr->DllBase + LayerExMovieLdr->SizeOfImage))
+				return TRUE;
+		}
+	}
+	return FALSE;
+}
+
+BOOL IsThreadValid()
+{
+	NTSTATUS                Status;
+	PVOID                   pvStart;
+	PLDR_MODULE             Ldr;
+	THREAD_START_PARAMETER *Parameter;
+
+	pvStart = NULL;
+	Ldr = GetModuleLdr(Nt_GetExeModuleHandle());
+	if (Ldr == NULL)
+		return FALSE;
+
+	Status = NtQueryInformationThread(Nt_CurrentTeb()->ClientId.UniqueThread, ThreadQuerySetWin32StartAddress, &pvStart, sizeof(pvStart), NULL);
+	if (NT_FAILED(Status))
+		return FALSE;
+
+	if (pvStart < Ldr->DllBase || pvStart > PtrAnd(Ldr->DllBase, Ldr->SizeOfImage))
+		return IsValidDllWorkerThread(pvStart);
+
+	//PrintConsoleW(L"New worker thread...\n");
+	Parameter = AllocateThreadParameter(NULL, (PVOID)((ULONG)NtAddAtom ^ (ULONG)Nt_CurrentPeb()));
+	RtlPushFrame(Parameter);
+	return TRUE;
+}
+
+//---------------------------------------------------------------------------
+HRESULT STDMETHODCALLTYPE IStreamAdapterXP3::Read(void *pv, ULONG cb, ULONG *pcbRead)
+{
+	//PrintConsole(L"%x, %x - %x\n", _ReturnAddress(), GetMainThread(), Nt_CurrentTeb()->ClientId.UniqueThread);
+
+	THREAD_START_PARAMETER *Parameter = (THREAD_START_PARAMETER *)Nt_FindThreadFrameByContext(THREAD_START_PARAMETER_MAGIC);
+	if (Parameter == NULL && IsThreadValid() == FALSE)
+	{	
+		PrintConsoleW(L"clear worker thread...\n");
+		Stream->Destroy();
+	}
+
+
+	try
+	{
+		ULONG read;
+		read = (ULONG)Stream->Read(pv, cb);
+		if (pcbRead) *pcbRead = read;
+	}
+	catch (...)
+	{
+		return E_FAIL;
+	}
+	return S_OK;
+}
+//---------------------------------------------------------------------------
+HRESULT STDMETHODCALLTYPE IStreamAdapterXP3::Write(const void *pv, ULONG cb,
+	ULONG *pcbWritten)
+{
+	try
+	{
+		ULONG written;
+		written = (ULONG)Stream->Write((PVOID)pv, cb);
+		if (pcbWritten) *pcbWritten = written;
+	}
+	catch (...)
+	{
+		return E_FAIL;
+	}
+	return S_OK;
+}
+//---------------------------------------------------------------------------
+HRESULT STDMETHODCALLTYPE IStreamAdapterXP3::Seek(LARGE_INTEGER dlibMove,
+	DWORD dwOrigin, ULARGE_INTEGER *plibNewPosition)
+{
+	try
+	{
+		switch (dwOrigin)
+		{
+		case STREAM_SEEK_SET:
+			if (plibNewPosition)
+				(*plibNewPosition).QuadPart =
+				Stream->Seek(dlibMove.QuadPart, TJS_BS_SEEK_SET);
+			else
+				Stream->Seek(dlibMove.QuadPart, TJS_BS_SEEK_SET);
+			break;
+		case STREAM_SEEK_CUR:
+			if (plibNewPosition)
+				(*plibNewPosition).QuadPart =
+				Stream->Seek(dlibMove.QuadPart, TJS_BS_SEEK_CUR);
+			else
+				Stream->Seek(dlibMove.QuadPart, TJS_BS_SEEK_CUR);
+			break;
+		case STREAM_SEEK_END:
+			if (plibNewPosition)
+				(*plibNewPosition).QuadPart =
+				Stream->Seek(dlibMove.QuadPart, TJS_BS_SEEK_END);
+			else
+				Stream->Seek(dlibMove.QuadPart, TJS_BS_SEEK_END);
+			break;
+		default:
+			return E_FAIL;
+		}
+	}
+	catch (...)
+	{
+		return E_FAIL;
+	}
+	return S_OK;
+}
+//---------------------------------------------------------------------------
+HRESULT STDMETHODCALLTYPE IStreamAdapterXP3::SetSize(ULARGE_INTEGER libNewSize)
+{
+	return E_NOTIMPL;
+}
+//---------------------------------------------------------------------------
+HRESULT STDMETHODCALLTYPE IStreamAdapterXP3::CopyTo(IStream *pstm, ULARGE_INTEGER cb,
+	ULARGE_INTEGER *pcbRead, ULARGE_INTEGER *pcbWritten)
+{
+	return E_NOTIMPL;
+}
+//---------------------------------------------------------------------------
+HRESULT STDMETHODCALLTYPE IStreamAdapterXP3::Commit(DWORD grfCommitFlags)
+{
+	return E_NOTIMPL;
+}
+//---------------------------------------------------------------------------
+HRESULT STDMETHODCALLTYPE IStreamAdapterXP3::Revert(void)
+{
+	return E_NOTIMPL;
+}
+//---------------------------------------------------------------------------
+HRESULT STDMETHODCALLTYPE IStreamAdapterXP3::LockRegion(ULARGE_INTEGER libOffset,
+	ULARGE_INTEGER cb, DWORD dwLockType)
+{
+	return E_NOTIMPL;
+}
+//---------------------------------------------------------------------------
+HRESULT STDMETHODCALLTYPE IStreamAdapterXP3::UnlockRegion(ULARGE_INTEGER libOffset,
+	ULARGE_INTEGER cb, DWORD dwLockType)
+{
+	return E_NOTIMPL;
+}
+//---------------------------------------------------------------------------
+HRESULT STDMETHODCALLTYPE IStreamAdapterXP3::Stat(STATSTG *pstatstg, DWORD grfStatFlag)
+{
+	// This method imcompletely fills the target structure, because some
+	// informations like access mode or stream name are already lost
+	// at this point.
+	
+	if (pstatstg)
+	{
+		ZeroMemory(pstatstg, sizeof(*pstatstg));
+
+		// pwcsName
+		// this object's storage pointer does not have a name ...
+		if (!(grfStatFlag &  STATFLAG_NONAME))
+		{
+			// anyway returns an empty string
+			LPWSTR str = (LPWSTR)CoTaskMemAlloc(sizeof(*str));
+			if (str == NULL) return E_OUTOFMEMORY;
+			*str = L'\0';
+			pstatstg->pwcsName = str;
+		}
+
+		// type
+		pstatstg->type = STGTY_STREAM;
+
+		// cbSize
+		pstatstg->cbSize.QuadPart = Stream->GetSize();
+
+		// mtime, ctime, atime unknown
+
+		// grfMode unknown
+		pstatstg->grfMode = STGM_DIRECT | STGM_READWRITE | STGM_SHARE_DENY_WRITE;
+		// Note that this method always returns flags above, regardless of the
+		// actual mode.
+		// In the return value, the stream is to be indicated that the
+		// stream can be written, but of cource, the Write method will fail
+		// if the stream is read-only.
+
+		// grfLockSuppoted
+		pstatstg->grfLocksSupported = 0;
+
+		// grfStatBits unknown
+	}
+	else
+	{
+		return E_INVALIDARG;
+	}
+
+	return S_OK;
+}
+//---------------------------------------------------------------------------
+HRESULT STDMETHODCALLTYPE IStreamAdapterXP3::Clone(IStream **ppstm)
+{
+	return E_NOTIMPL;
+}
---
+++ Patch/EmptyXEmbryo_chs/Loader/Loader.cpp
@@ -0,0 +1,152 @@
+#include "my.h"
+
+#pragma comment(linker, "/ENTRY:MainEntry")
+#pragma comment(linker, "/SECTION:.text,ERW /MERGE:.rdata=.text /MERGE:.data=.text")
+#pragma comment(linker, "/SECTION:.Xmoe,ERW /MERGE:.text=.Xmoe")
+
+#pragma comment(lib, "ntdll.lib")
+
+typedef
+BOOL
+(WINAPI
+*FuncCreateProcessInternalW)(
+HANDLE                  hToken,
+LPCWSTR                 lpApplicationName,
+LPWSTR                  lpCommandLine,
+LPSECURITY_ATTRIBUTES   lpProcessAttributes,
+LPSECURITY_ATTRIBUTES   lpThreadAttributes,
+BOOL                    bInheritHandles,
+ULONG                   dwCreationFlags,
+LPVOID                  lpEnvironment,
+LPCWSTR                 lpCurrentDirectory,
+LPSTARTUPINFOW          lpStartupInfo,
+LPPROCESS_INFORMATION   lpProcessInformation,
+PHANDLE                 phNewToken
+);
+
+BOOL
+(WINAPI
+*StubCreateProcessInternalW)(
+HANDLE                  hToken,
+LPCWSTR                 lpApplicationName,
+LPWSTR                  lpCommandLine,
+LPSECURITY_ATTRIBUTES   lpProcessAttributes,
+LPSECURITY_ATTRIBUTES   lpThreadAttributes,
+BOOL                    bInheritHandles,
+ULONG                   dwCreationFlags,
+LPVOID                  lpEnvironment,
+LPCWSTR                 lpCurrentDirectory,
+LPSTARTUPINFOW          lpStartupInfo,
+LPPROCESS_INFORMATION   lpProcessInformation,
+PHANDLE                 phNewToken
+);
+
+BOOL
+WINAPI
+VMeCreateProcess(
+HANDLE                  hToken,
+LPCWSTR                 lpApplicationName,
+LPWSTR                  lpCommandLine,
+LPCWSTR                 lpDllPath,
+LPSECURITY_ATTRIBUTES   lpProcessAttributes,
+LPSECURITY_ATTRIBUTES   lpThreadAttributes,
+BOOL                    bInheritHandles,
+ULONG                   dwCreationFlags,
+LPVOID                  lpEnvironment,
+LPCWSTR                 lpCurrentDirectory,
+LPSTARTUPINFOW          lpStartupInfo,
+LPPROCESS_INFORMATION   lpProcessInformation,
+PHANDLE                 phNewToken
+)
+{
+	BOOL             Result, IsSuspended;
+	UNICODE_STRING   FullDllPath;
+
+	RtlInitUnicodeString(&FullDllPath, lpDllPath);
+
+	StubCreateProcessInternalW =
+		(FuncCreateProcessInternalW)Nt_GetProcAddress(GetKernel32Handle(),
+		"CreateProcessInternalW");
+
+	IsSuspended = !!(dwCreationFlags & CREATE_SUSPENDED);
+	dwCreationFlags |= CREATE_SUSPENDED;
+	Result = StubCreateProcessInternalW(
+		hToken,
+		lpApplicationName,
+		lpCommandLine,
+		lpProcessAttributes,
+		lpThreadAttributes,
+		bInheritHandles,
+		dwCreationFlags,
+		lpEnvironment,
+		lpCurrentDirectory,
+		lpStartupInfo,
+		lpProcessInformation,
+		phNewToken);
+
+	if (!Result)
+		return Result;
+
+	InjectDllToRemoteProcess(
+		lpProcessInformation->hProcess,
+		lpProcessInformation->hThread,
+		&FullDllPath,
+		IsSuspended
+		);
+
+	NtResumeThread(lpProcessInformation->hThread, NULL);
+
+	return TRUE;
+}
+
+LPWSTR NTAPI StringCatW(LPWSTR lpString1, LPCWSTR lpString2)
+{
+	ULONG_PTR Length;
+
+	Length = StrLengthW(lpString1);
+	StrCopyW(&lpString1[Length], lpString2);
+	return lpString1;
+}
+
+
+
+INT CDECL MainEntry()
+{
+	WCHAR               DllPath[MAX_PATH];
+	STARTUPINFOW        si;
+	PROCESS_INFORMATION pi;
+	BOOL                Result;
+
+	RtlZeroMemory(&si, sizeof(si));
+	RtlZeroMemory(&pi, sizeof(pi));
+	si.cb = sizeof(si);
+
+	RtlZeroMemory(DllPath, MAX_PATH * 2);
+	Nt_SetExeDirectoryAsCurrent();
+	Nt_GetCurrentDirectory(MAX_PATH, DllPath);
+	StringCatW(DllPath, L"\\");
+
+	if (Nt_GetModuleHandle(L"LocaleEmulator.dll") || Nt_GetModuleHandle(L"ntleai.dll"))
+	{
+		MessageBoxW(NULL, L"請勿開啟轉區程式...", L">_<", MB_OK | MB_ICONWARNING);
+		Ps::ExitProcess(0);
+	}
+
+	if (Nt_CurrentPeb()->OSMajorVersion == 5)
+	{
+		MessageBoxW(NULL, L"抱歉，本补丁不再支持Windows XP\n以及Windows 2003相关系列的操作系统", L">_<", MB_OK | MB_ICONWARNING);
+		Ps::ExitProcess(0);
+	}
+
+	StringCatW(DllPath, L"EmptyXEmbryo_chs.dll");
+
+	Result = VMeCreateProcess(NULL, NULL, L"Ｅ×Ｅ.exe", DllPath, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi, NULL);
+
+	if (Result == FALSE)
+	{
+		MessageBoxW(NULL, L"游戏启动失败，也许是防护软件拦截所致。\n同时请检查文件完整性和补丁存放目录是否正确。", L"游戏启动失败", MB_OK | MB_ICONERROR);
+	}
+	Ps::ExitProcess(0);
+	return Result;
+}
+
